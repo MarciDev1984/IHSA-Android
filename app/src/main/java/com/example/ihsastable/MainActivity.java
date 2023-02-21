0604b351dc806fa4d3e0c03b02d5e8a1e6f8e6c8
JacobPickman <78869483+JacobPickman@users.noreply.github.com>
2023-02-21 14:13:56 -0600

    Added onClick to RecyclerView

--- app/src/main/java/com/example/ihsastable/MainActivity.java
+++ app/src/main/java/com/example/ihsastable/MainActivity.java
@@ -5,11 +5,39 @@ import androidx.core.view.GestureDetectorCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private class RecyclerViewOnGestureListener extends GestureDetector.SimpleOnGestureListener {
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            RecyclerView showRV = findViewById(R.id.showsRV);
+            View view = showRV.findChildViewUnder(e.getX(), e.getY());
+            Log.d("click", "click happened");
+
+            if (view != null) {
+                RecyclerView.ViewHolder holder = showRV.getChildViewHolder(view);
+
+                if (holder instanceof showAdapter.showViewHolder) {
+                    int position = holder.getAdapterPosition();
+
+                    Log.d("click", "single tap clicked on item " + position);
+                    //Intent goToNextActivity = new Intent(getApplicationContext(), testActivity.class);
+                    //startActivity(goToNextActivity);
+                    Log.d("click", "Going to show");
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 
     private showAdapter showServer;
 
@@ -24,6 +52,17 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayoutManager myManager = new LinearLayoutManager(this);
         showRecycler.setLayoutManager(myManager);
+
+        GestureDetectorCompat detector = new GestureDetectorCompat(this,
+                new RecyclerViewOnGestureListener());
+        showRecycler.addOnItemTouchListener(
+                new RecyclerView.SimpleOnItemTouchListener() {
+                    @Override
+                    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                        return detector.onTouchEvent(e);
+                    }
+                });
+
 
     }
 }

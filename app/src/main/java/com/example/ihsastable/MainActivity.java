0b2a7c3b246836da9b7b87a75cac939ae53ed9b3
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2023-02-23 18:58:44 -0600

    Clean up code
    Preparing to test tabbed app and move RV

--- app/src/main/java/com/example/ihsastable/MainActivity.java
+++ app/src/main/java/com/example/ihsastable/MainActivity.java
@@ -6,15 +6,44 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private class RecyclerViewOnGestureListener extends GestureDetector.SimpleOnGestureListener
+    {
+        public boolean onSingleTapConfirmed(MotionEvent e)
+        {
+            RecyclerView showRV = findViewById(R.id.showsRV);
+            View view = showRV.findChildViewUnder(e.getX(), e.getY());
+            Log.d("click", "click happened");
 
+            if (view != null)
+            {
+                RecyclerView.ViewHolder holder = showRV.getChildViewHolder(view);
+
+                if (holder instanceof showAdapter.showViewHolder)
+                {
+                    int position = holder.getAdapterPosition();
+                    Log.d("click", "single tap clicked on item " + position);
+                    //Intent goToNextActivity = new Intent(getApplicationContext(), testActivity.class);
+                    //startActivity(goToNextActivity);
+                    Log.d("click", "Going to show");
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 
     private showAdapter showServer;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -25,5 +54,14 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager myManager = new LinearLayoutManager(this);
         showRecycler.setLayoutManager(myManager);
 
+        GestureDetectorCompat detector = new GestureDetectorCompat(this, new RecyclerViewOnGestureListener());
+        showRecycler.addOnItemTouchListener(new RecyclerView.SimpleOnItemTouchListener()
+        {
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e)
+            {
+                return detector.onTouchEvent(e);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ihsastable/showModel.java
+++ app/src/main/java/com/example/ihsastable/showModel.java
@@ -2,54 +2,74 @@ package com.example.ihsastable;
 
 import java.util.ArrayList;
 
-public class showModel {
-
-    public static class Show{
+public class showModel
+{
+    public static class Show
+    {
         private String showname;
-
-        public Show(String showname) {
+        public Show(String showname)
+        {
             this.showname = showname;
         }
-
-        public String getShow() {
+        public String getShow()
+        {
             return showname;
         }
-
     }
 
     private ArrayList<Show> showList;
 
-    private showModel (){
+    private showModel ()
+    {
         showList = new ArrayList<Show>();
         loadInitialTasks();
     }
 
     public void loadInitialTasks() {
-        Show item1 = new Show("H");
+        Show item1 = new Show("Show 1");
+        Show item2 = new Show("Show 2");
+        Show item3 = new Show("Show 3");
+        Show item4 = new Show("Show 4");
+        Show item5 = new Show("Show 5");
+        Show item6 = new Show("Show 6");
+        Show item7 = new Show("Show 7");
+        Show item8 = new Show("Show 8");
+        Show item9 = new Show("Show 9");
+        Show item10 = new Show("Show 10");
+        Show item11 = new Show("Show 11");
+        Show item12 = new Show("Show 12");
+        Show item13 = new Show("Show 13");
+        Show item14 = new Show("Show 14");
+        Show item15 = new Show("Show 15");
         showList.add(item1);
-        Show item2 = new Show("O");
         showList.add(item2);
-        Show item3 = new Show("R");
         showList.add(item3);
-        Show item4 = new Show("S");
         showList.add(item4);
-        Show item5 = new Show("E");
         showList.add(item5);
-        Show item6 = new Show("God Country");
         showList.add(item6);
-
+        showList.add(item7);
+        showList.add(item8);
+        showList.add(item9);
+        showList.add(item10);
+        showList.add(item11);
+        showList.add(item12);
+        showList.add(item13);
+        showList.add(item14);
+        showList.add(item15);
     }
 
-    public ArrayList<Show> getTaskArray() {
+    public ArrayList<Show> getTaskArray()
+    {
         return showList;
     }
 
     public static showModel theModel = null;
-    public static showModel getSingleton(){
-        if(theModel == null){
+    public static showModel getSingleton()
+    {
+        if(theModel == null)
+        {
             theModel = new showModel();
         }
         return theModel;
     }
-
 }

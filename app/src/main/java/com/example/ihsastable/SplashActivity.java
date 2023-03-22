3b886953051846f0146b2c60c540f656d359a22b
fisherreese2R <s540308@nwmissouri.edu>
2023-03-22 00:43:23 -0500

    splash screen is in beta stages still working out how to get IHSA logo to work

---
+++ app/src/main/java/com/example/ihsastable/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.ihsastable;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 3000);
+
+    }
+}
